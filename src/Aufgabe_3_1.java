773d6c9b6bb629ea565f73d93b34d9779dd9e672
jpohl <jpohl@web.de>
2024-09-30 18:48:00 +0200

    Aufgabe 1

--- src/Aufgabe_3_1.java
+++ src/Aufgabe_3_1.java
@@ -2,26 +2,68 @@ public class Aufgabe_3_1 {
 
     // Methode fuellt das Feld mit Zufallszahlen aus derm Intervall [1,100]
     public static void fillRand(int[] feld) {
-	for (int i=0;i<feld.length;i++){
-	    feld[i] = (int) (Math.random() * 100) + 1;
-	}
+        for (int i = 0; i < feld.length; i++) {
+            feld[i] = (int) (Math.random() * 100) + 1;
+
+        }
     }
 
-    public static void main(String[] args){
-	int [] feld = new int[10];
-	fillRand(feld);
+    public static void main(String[] args) {
+        int[] feld = new int[10];
+        fillRand(feld);
 
-	/* *** Aufgabenteil (a) *** */
+        /* *** Aufgabenteil (a) *** */
         // 1.5
 
-	
-	/* *** Aufgabenteil (b) *** */
+        for (int i = 0; i < feld.length; i++) {
+            System.out.print(feld[i]);
+            // kein "-" nach der 10. Zahl
+            if (i < 9) {
+                System.out.print("-");
+            }
+        }
+        System.out.println();
+
+        /* *** Aufgabenteil (b) *** */
+        int gerade = 0, ungerade = 0, differenz = 0;
         // 3
+        for (int j : feld) {
+            if (j % 2 == 0) {
+                gerade++;
+            } else ungerade++;
+        }
+        System.out.println("Gerade Zahlen: " + gerade + " ungerade Zahlen:  " + ungerade);
 
+        if (gerade == ungerade) {
+            System.out.println("Das Feld enthaelt gleich viele gerade und ungerade Zahlen");
+        } else if (gerade > ungerade) {
+            differenz = gerade - ungerade;
+            System.out.println("Das Feld enthaelt " + differenz + " gerade Zahlen mehr als ungerade Zahlen.");
+        } else {
+            differenz = ungerade - gerade;
+            System.out.println("Das Feld enthaelt " + differenz + " gerade Zahlen weniger als ungerade Zahlen.");
+        }
 
-	/* *** Aufgabenteil (c) *** */
+        /* *** Aufgabenteil (c) *** */
         // 2.5
-	
+
+        int maxDiff = 0;
+        int indexMaxDiff = 0;
+
+        for (int i = 0; i < feld.length - 1; i++) {
+            int diff = Math.abs(feld[i] - feld[i + 1]);
+            if (diff > maxDiff) {
+                maxDiff = diff;
+                indexMaxDiff = i;
+            }
+        }
+
+        // Ausgabe der maximalen Differenz
+        System.out.println("Die max. Differenz von " + maxDiff + " tritt auf zwischen feld[" + indexMaxDiff + "] und feld[" + (indexMaxDiff + 1) + "].");
+
     }
 }
+
+
+
 
